64cb84c47d5da837c1cad250628fda23634b8ad4
chris veigl <veigl@technikum-wien.at>
2020-02-21 14:43:22 +0100

    changes for fixing cylon firmata handling

--- backend/robot.js
+++ backend/robot.js
@@ -5,63 +5,66 @@ const socket = io('ws://localhost:4444');
 var robot = Cylon.robot({
 
   connections: {
-    //arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
-    loopback: { adaptor: 'loopback' }
+    arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+    //loopback: { adaptor: 'loopback' }
   },
 
   devices: {
-   /*led: { driver: 'led', pin: 13 },
+   led: { driver: 'led', pin: 13 },
     goal1: { driver: 'button', pin: 12 },
     goal2: { driver: 'button', pin: 10 },
     led1: { driver: 'led', pin: 8 },
     led2: { driver: 'led', pin: 9 },
     button1: { driver: 'button', pin: 6 },
-    button2: { driver: 'button', pin: 7 }*/
+    button2: { driver: 'button', pin: 7 }
   },
 
-  work: function() {
+
+  work: function(my) {
     let lastPressTimeRed = 1;
     let lastPressTimeBlue = 1;
     const intervall = 5;
-    every((0.5).second(), function() {
+  /*  every((0.5).second(), function() {
       const goal = ['red', 'blue'];
       const color = goal[Math.floor(Math.random() * Math.floor(2))];
       socket.emit('goal',color);
+    });*/
+
+       
+    my.button1.on('press', function(){
+      lastPressTimeRed = Math.floor(Date.now()/1000);
     });
 
-      /*  
-    button1.on('press'){
-      lastPressTimeRed = Math.floor(Date.now()/1000);
-    }
-
-    button2.on('press'){
+    my.button2.on('press', function(){
       lastPressTimeBlue = Math.floor(Date.now()/1000);
-    }
+    });
 
-    button1.on('release'){
-      if(Date.now()/1000 > lastPressTimeRed + intervall){
+    my.button1.on('release', function(){
+      //if(Date.now()/1000 > lastPressTimeRed + intervall){
+        /*
         socket.emit('canelDummyGame');
-      } else {
-        this.socketService.emit('buttonClick', 'red');
-      };
-    }
+      } else {*/
+        socket.emit('buttonClick', 'red');
+      //};
+    });
 
-    button2.on('release'){
+    my.button2.on('release', function(){
+      /*
       if(Date.now()/1000 > lastPressTimeBlue + intervall){
         socket.emit('canelDummyGame');
-      } else {
-        this.socketService.emit('buttonClick', 'blue');
-      };
-    } 
-
-    goal1.on('push', function () {
-      socket.emit('goal');
+      } else {*/
+        socket.emit('buttonClick', 'blue');
+      //};
     });
 
-    goal2.on('push', function () {
-      socket.emit('goal');
+    my.goal1.on('push', function () {
+      socket.emit('goal', 'red');
     });
-    */
+
+    my.goal2.on('push', function () {
+      socket.emit('goal', 'blue');
+    });
+
 
   }
 
